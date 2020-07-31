d865ec2501882586b7ddbcc92d704e18a6aa9c09
Carter Konz <carter.konz@gmail.com>
2020-07-31 12:11:18 -0500

    conversion script v1

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Readme
+
+## Usage
+Requires Python 3
+
+- 1. Copy `.tsv` files to the `tsv` folder.
+- 2. Use a terminal to navigate to the project directory and run the python script.
+- 3. Converted `.csv` files will end up in the `csv` folder.
+
+### macOS
+
+```shell
+cd ~/Downloads/tsv-converter
+python tsv-converter.py
+```
+
+### Windows
+
+```shell
+cd ~/Downloads/tsv-converter
+Python tsv-converter.py
+```
---
+++ tsv-converter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import glob
+import csv
+import sys
+import os
+
+# Create glob pattern, fetch filenames
+if len(sys.argv) < 2:
+    pattern = os.path.join('./tsv').rstrip('/') + '/*.tsv'
+else:
+    pattern = sys.argv[1].rstrip('/') + '/*.tsv'
+
+filenames = glob.glob(pattern)
+
+# Loop through, and convert
+for i, source_filename in enumerate(filenames):
+    csv.field_size_limit(sys.maxsize)
+    
+    print(source_filename)
+    print(source_filename.rstrip('.tsv'))
+    dest_filename = os.path.join('.', 'csv', os.path.basename(source_filename).rstrip('.tsv')) + '.csv'
+
+    with open(source_filename) as source_tsv:
+        reader = csv.reader(source_tsv, delimiter='\t', quotechar='|')
+        with open(dest_filename, 'w', newline='') as dest_csv:
+            writer = csv.writer(dest_csv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+            for row in reader:
+                writer.writerow(row)
+
+    i += 1
+    print('Created: ' + os.path.basename(dest_filename))
+
+print('\n', i, 'files have been converted...\n')
